70cfae937c652c57bb18e63a304ee05373a550bd
Tome Trencev <trencevtome@yahoo.com>
2022-02-02 22:08:54 +0100

    domasno

---
+++ d5/routes/index.js
@@ -0,0 +1,24 @@
+const router = require ("express").Router();
+
+router.get  ("/home",(req, res)=>{
+      return res
+      .status(200)
+      .json("WELCOME ON MY PAGE!")
+});
+
+router.post  ("/register", (req, res)=>{
+    if(req.body.password){
+        return res.status(201).json("You are Logged in")
+    }
+    return res.status(401).json("What is your password")
+})
+
+router.use ((req, res)=>{
+    return res.status(404).json("not found!")
+})
+
+
+
+
+
+module.exports = router;
