61fc62bab81c9d32c4dccf396906a2a61df750a4
Tome Trencev <trencevtome@yahoo.com>
2022-02-20 19:27:19 +0100

    domasno

--- d10/controllers/products.js
+++ d10/controllers/products.js
@@ -11,7 +11,7 @@ const createProduct = async (req,res,next)=>{
         return res.status(400).json('Please instert product name and quantity')
     }
     try {
-        await Product.create({productName, productQuantity});
+        await Product.insertMany({productName, productQuantity});
         return res.status(201).json("product created !")
     } catch (error) {
         return res.status(500).json(error)
--- d10/routes/index.js
+++ d10/routes/index.js
@@ -13,12 +13,12 @@ const {authMiddleware}= require("../middlewares/jwt");
 
 const PREFIX = process.env.API_PREFIX;
 
-router.post(PREFIX + '/register',authMiddleware, registerUser);
-router.post(PREFIX + '/login',authMiddleware, login);
-router.post(PREFIX + '/products',createProduct );
-router.get(PREFIX + ' /products', getAllProducts);
-router.put(PREFIX + '/products/:_id',updateProduct);
-router.delete(PREFIX + '/products/:_id', deleteProduct)
+router.post(PREFIX + '/register', registerUser);
+router.post(PREFIX + '/login', login);
+router.post(PREFIX + '/products',authMiddleware, createProduct );
+router.get(PREFIX + ' /products',authMiddleware, getAllProducts);
+router.put(PREFIX + '/products/:_id', authMiddleware,updateProduct);
+router.delete(PREFIX + '/products/:_id',authMiddleware, deleteProduct)
 
 router.use((req,res)=>{
     return res.status(404).json("Not Found!");
